d695d30782e21e51fd24fb64f5b49674ffa51e22
Samuel Reeder <samuel.reeder8@gmail.com>
2023-09-25 10:58:14 -0400

    get avg grades but only works if everyone input grade

--- src/main/java/use_case/GetAverageGradeUseCase.java
+++ src/main/java/use_case/GetAverageGradeUseCase.java
@@ -11,7 +11,30 @@ public final class GetAverageGradeUseCase {
     }
 
     public float getAverageGrade(String course) {
-        // TODO: Get average grade for all students in your team.
-        return 0.0f;
+        String[] teamMembers = gradeDB.getMyTeam().getMembers();
+
+        if (teamMembers == null || teamMembers.length == 0) {
+            return 0.0f;
+        }
+
+        float totalGrade = 0.0f;
+        int count = 0;
+
+        for (String member : teamMembers) {
+            Grade grade = gradeDB.getGrade(member, course);
+            System.out.println(member);
+            if (grade != null) {
+                totalGrade += grade.getGrade();
+                System.out.println(grade.getGrade());
+                count++;
+            }
+        }
+
+        if (count == 0) {
+            return 0.0f;
+        }
+
+        return totalGrade / count;
+
     }
 }
